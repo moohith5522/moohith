1fa251935a5f738bfa278fcb6a3aa1c4c101044b
moohith5522 <lathika.ad21@bitsathy.ac.in>
2025-06-19 15:57:52 +0530

    Add person object and newDetails function with additional properties

---
+++ objc.js
@@ -0,0 +1,12 @@
+let person={
+    name: "John",
+    age: 30,
+    city: "New York"
+};
+let newDetails = function() {
+    ...person
+    role: "Developer",
+    company: "TechCorp"
+    isMarried: false
+    salary: 60000
+};
